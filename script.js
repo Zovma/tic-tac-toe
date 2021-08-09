6121a644ce31bfc7c9737893361f298bc523a5ec
Zovma <dim.borzov@gmail.com>
2021-08-09 11:46:16 +0300

    'first'

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.onload = function () {
+//     document.getElementById('first').onclick = chenge
+// }
+// function chenge() {
+//     document.getElementById('first').style.backgroundColor = 'green'
+// }
+
