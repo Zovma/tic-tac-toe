722ad11875d413872bebec4d5dadf39b8c689ded
Zovma <dim.borzov@gmail.com>
2021-08-20 22:52:36 +0300

    'new'

--- script.js
+++ script.js
@@ -1,77 +1,54 @@
-// let zero = document.getElementById('zero')
-// let one = document.getElementById('one')
-// let two = document.getElementById('two')
-// let three = document.getElementById('three')
-// let four = document.getElementById('four')
-// let five = document.getElementById('five')
-// let six = document.getElementById('six')
-// let seven = document.getElementById('seven')
-// let eight = document.getElementById('eight')
-// let square = document.getElementById('square')
-// zero.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// .addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-// one.addEventListener('click', nol1)
-square.addEventListener('click', nol1)
+square.addEventListener('click', check)
+let counter = 0
+function check(e) {
+    console.log('click');
+    let click = e.path[0]
+    console.log(click.nodeName);
+
+    if (click.nodeName === 'DIV') {
+        nol1(click)
+    }
+    else {
+        console.log('условие не прошло');
+        e.preventDefault()
+    }
+}
+
 
 function nol1(e) {
+    let elem = e
+    // if (!elem.firstChild) {
     let nol = document.createElement('IMG')
     nol.style.width = '14em'
     nol.style.height = '14em'
     nol.src = './img/nol.png'
-    let elem = e.path[0]
     elem.appendChild(nol);
-    console.log(e.path[0]);
-    square.removeEventListener('click', nol1)
-    square.addEventListener('click', cross1)
-    console.log('+');
+    // console.log('X');
+    // square.removeEventListener('click', nol1)
+    // square.addEventListener('click', cross1)
+    // }
+    // else {
+    //     console.log('return x');
+    //     return
+    // }
 }
 
-function cross1 (e) {
+
+
+function cross1(e) {
+    let elem = e
+    // if (!elem.firstChild) {
     let cross = document.createElement('IMG')
     cross.style.width = '14em'
     cross.style.height = '14em'
     cross.src = './img/krest.png'
-    let elem = e.path[0]
     elem.appendChild(cross)
-    console.log(e.path[0])
-    square.removeEventListener('click', cross1)
-    square.addEventListener('click', nol1)
-
+    // console.log('0');
+    //     square.removeEventListener('click', cross1)
+    //     square.addEventListener('click', nol1)
+    // }
+    // else {
+    //     console.log('return 0');
+    //     return
+    // }
 }
-
-
-// zero.addEventListener('mouseout',
-//     img_x)
-// zero.addEventListener("mouseout", function (e) {
-//     img_x(), false
-// })
-
-
-// function img(e) {
-//     let nol = document.createElement('IMG')
-//     nol.style.width = '14em'
-//     nol.style.height = '14em'
-//     nol.src = './img/nol.png'
-//     zero.appendChild(nol);
-//     console.log('+');
-//     zero.removeEventListener("mouseover",
-//         img)
-// // }
-
-
-// function img_x() {
-//     while (zero.firstChild) {
-//         zero.removeChild(zero.firstChild);
-//         console.log('-')
-//         zero.addEventListener("mouseover",
-//             img)
-//     }
-// }
-
-
